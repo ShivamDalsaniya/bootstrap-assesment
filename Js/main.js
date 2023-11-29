292616a3ff5947024ad0da6b0f886122c6880409
ShivamDalsaniya <dalsaniashivam234@gmail.com>
2023-11-29 16:15:13 +0530

    Assesment

---
+++ Js/main.js
@@ -0,0 +1,14 @@
+// const humbgger = document.querySelector(".hubgger-btn");
+// const menu = document.querySelector("ul");
+// const first=document.getElementsByClassName('first');
+
+
+
+
+// humbgger.addEventListener("click" , () => {
+//      console.log('clicked');
+//      // first[0].classList.remove('hidden');
+//      first[0].classList.toggle("active");
+//      menu.classList.toggle("active");
+
+// })$(document).ready(function{})
